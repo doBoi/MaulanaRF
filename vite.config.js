d6bc2c2610ec9ebd081fa825ce26f4dd273b0d17
Maulana Ridho Farhansyah <78261721+doBoi@users.noreply.github.com>
2024-02-05 21:16:09 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/",
-=======
-  base: "/PORTO-MAUL/",
->>>>>>> 05b61e6bfc1dd9e9c93d18526ba5d37c4ce1301c
   define: {
     'process.env': {}
   }
