c3b07a9ab7afd41dc54c4a4f9c592e69003f5a57
doBoi <maulana4810@gmail.com>
2024-02-05 21:29:18 +0700

    fix porto

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import porto from "./data/porto";
 import Navigation from "./components/Navigation";
 import Banner from "./components/Banner";
 import About from "./components/About";
-import Portofolio from "./components/portofolio";
+import Portofolio from "./components/Portofolio";
 import Contact from "./components/Contact";
 function App() {
   const [portos, setPortos] = useState(porto);
@@ -12,7 +12,7 @@ function App() {
       <Navigation ></Navigation>
       <Banner ></Banner>
       <About></About>
-      <Portofolio></Portofolio>
+      <Portofolio portos={portos}></Portofolio>
       <Contact></Contact>
     </div>
   )
