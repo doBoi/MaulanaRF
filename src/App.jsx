ed83827d388b70a0e19adfd4e13c7ea655e66d26
doBoi <maulana4810@gmail.com>
2024-02-05 21:14:41 +0700

    add rollup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
+import porto from "./data/porto";
 import Navigation from "./components/Navigation";
 import Banner from "./components/Banner";
 import About from "./components/About";
 import Portofolio from "./components/portofolio";
 import Contact from "./components/Contact";
-import porto from "./data/porto";
 function App() {
   const [portos, setPortos] = useState(porto);
   return (
